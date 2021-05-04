24706cb6aa7499d1d15db8ff2f4fa5d9115bf8d1
SileLence <smkslnc@gmail.com>
2021-05-04 23:08:23 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Trunov");
+        System.out.println("14.02.1990");
+    }
+}
