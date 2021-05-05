ae3a14e6819256245e70e19eff49cff7e2b503e5
SileLence <smkslnc@gmail.com>
2021-05-06 00:15:28 +0300

    7. Расстояние между точками в системе координат [#188]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -10,10 +10,10 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        System.out.println("Distance between points (0, 0) and (2, 0) equals: " + Point.distance(0,0,2,0));
-        System.out.println("Distance between points (4, -2) and (5, 7) equals: " + Point.distance(4,-2,5,7));
-        System.out.println("Distance between points (0, 0) and (0, 0) equals: " + Point.distance(0,0,0,0));
-        System.out.println("Distance between points (-14, -3) and (7, 35) equals: " + Point.distance(-14,-3,7,35));
-        System.out.println("Distance between points (-1, -2) and (-5, -5) equals: " + Point.distance(-1,-2,-5,-5));
+        System.out.println("Distance between points (0, 0) and (2, 0) equals: " + Point.distance(0, 0, 2, 0));
+        System.out.println("Distance between points (4, -2) and (5, 7) equals: " + Point.distance(4, -2, 5, 7));
+        System.out.println("Distance between points (0, 0) and (0, 0) equals: " + Point.distance(0, 0, 0, 0));
+        System.out.println("Distance between points (-14, -3) and (7, 35) equals: " + Point.distance(-14, -3, 7, 35));
+        System.out.println("Distance between points (-1, -2) and (-5, -5) equals: " + Point.distance(-1, -2, -5, -5));
     }
 }
