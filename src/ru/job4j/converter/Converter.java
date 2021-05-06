913271815598040f217bdc25add6dfa906da9300
SileLence <smkslnc@gmail.com>
2021-05-06 08:18:16 +0300

    5. Конвертер валюты [#41583 #722]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -11,12 +11,10 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        int dollar = Converter.rubleToDollar(275);
+        int euro = Converter.rubleToEuro(1632);
+        int dollar = Converter.rubleToDollar(784);
 
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("275 rubles are " + dollar + " dollars.");
-        System.out.println("1 euro are " + 70 / 60 + " dollars.");
-        System.out.println("1 dollar are " + 60 / 70 + " euro.");
+        System.out.println("1632 rubles are " + euro + " euro.");
+        System.out.println("784 rubles are " + dollar + " dollars.");
     }
 }
