c9ceb1abf75380804b809dc104ae06de0bda57b0
SileLence <smkslnc@gmail.com>
2021-05-15 13:52:37 +0300

    7. Арифметические операции.

---
+++ src/ru/job4j/condition/ArithmeticActions.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class ArithmeticActions {
+    public static String selectAction(int left, int right, int rsl) {
+        if (rsl == left + right) return "added";
+        if (rsl == left - right) return "subtracted";
+        if (rsl == left * right) return "multiplied";
+        if (rsl == left / right) return "divided";
+        return "none";
+    }
+}
