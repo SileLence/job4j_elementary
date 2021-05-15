bbafdffcb681c588766968952e9e1db1b85be7fb
SileLence <smkslnc@gmail.com>
2021-05-15 14:07:41 +0300

    8. Hello World

---
+++ src/ru/job4j/condition/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class HelloWorld {
+    public static String checkNumber(int number) {
+        boolean divideBy3 = number % 3 == 0;
+        boolean divideBy5 = number % 5 == 0;
+
+        if (divideBy3 && divideBy5) {
+            return "Hello, World!!!";
+        } else if (divideBy3) {
+            return  "Hello";
+        } else if (divideBy5) {
+            return "World";
+        }
+        return "Operation not support";
+    }
+}
