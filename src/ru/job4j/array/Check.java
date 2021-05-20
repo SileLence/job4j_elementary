310e5966ff0ce233e8d86368f96ff5582e774a2b
SileLence <smkslnc@gmail.com>
2021-05-21 00:10:27 +0300

    6.3. Массив заполнен true или false [#53857]

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean reference = data[0];
+
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] != reference) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ test/ru/job4j/array/CheckTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        boolean[] data = {true, true, true};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        boolean[] data = {false, true, true};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        boolean[] data = {false, false, false};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+}
