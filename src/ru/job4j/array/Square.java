d6599af056c100da380b72aa7495405315958d28
SileLence <smkslnc@gmail.com>
2021-05-20 21:31:26 +0300

    6.0.5. Массивы в тестах. [#226789]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -9,11 +9,4 @@ public class Square {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        int[] array = calculate(4);
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
-        }
-    }
 }
