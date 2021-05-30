208ad88624a238c7418c6f2091aa764756543c20
SileLence <smkslnc@gmail.com>
2021-05-31 00:01:03 +0300

    50. Циклический сдвиг вправо

---
+++ src/ru/job4j/array/RightShift.java
@@ -0,0 +1,31 @@
+
+/*
+* Ваша задача выполнить циклический сдвиг вправо с шагом count
+*/
+
+package ru.job4j.array;
+
+public class RightShift {
+
+    public static void shift(int[] nums, int count) {
+        for (int i = 0; i < count; i++) {
+            shift(nums);
+        }
+    }
+
+    // метод делает сдвиг с шагом 1
+    private static void shift(int[] nums) {
+        int lastElement = nums[nums.length - 1];
+        int temp1 = nums[0];
+        int temp2 = nums[1];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = temp1;
+            temp1 = temp2;
+            if (i + 1 != nums.length) {
+                temp2 = nums[i + 1];
+            }
+        }
+        nums[0] = lastElement;
+    }
+}
